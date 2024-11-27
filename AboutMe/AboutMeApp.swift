b7fde6107c3b4e9c75570bd01ce38529d42871b4
edy alves <30.edyalves@gmail.com>
2024-11-27 19:27:07 -0300

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by ednardo alves on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
