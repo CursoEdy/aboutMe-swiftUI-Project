d59b39b9d07cac0d65fa3dee6e8a04a866b69df6
edy alves <30.edyalves@gmail.com>
2024-11-28 09:46:19 -0300

    refatorando aboutMeDetail, List e Image

---
+++ AboutMe/Views/AboutMeDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutMeDetailView.swift
+//  AboutMe
+//
+//  Created by ednardo alves on 27/11/24.
+//
+
+import SwiftUI
+
+struct AboutMeDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutMeDetailView()
+}
